53e79442c5d787fc3712e5bca50a9d32e8c11f95
Kseniia <balerinagizelle@mail.ru>
2022-01-07 16:54:25 +0300

    Add project files.

---
+++ Lab23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+        static void Factorial (int n)
+        { int a = 1;
+            for (int i = 1; i < n; i++)
+            {
+                a *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(a);
+        }
+        static async void FactorialAsync(int n)
+        { await Task.Run(() => Factorial(n)); }
+
+    }
+}
